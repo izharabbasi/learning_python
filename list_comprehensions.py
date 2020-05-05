cb3cf7200569e5331a3e468bd7c730aa57fcf8a0
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 07:08:40 +0500

    add list to another list

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,7 +1,16 @@
 
-square = []
+# square = []
 
-for i in range(1,11):
-    square.append(i**2)
+# for i in range(1,11):
+#     square.append(i**2)
 
-print(square)
+# print(square)
+
+num = range(1,11)
+num1 = []
+for n in num:
+    num1.append(n**3)
+
+print(num1)
+
+
