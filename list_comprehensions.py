41ee657c52f7ba6249c008bda10f3556ad1d8221
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 07:13:09 +0500

    add list comprehension in lists

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -6,11 +6,22 @@
 
 # print(square)
 
-num = range(1,11)
-num1 = []
-for n in num:
-    num1.append(n**3)
+# num = range(1,11)
+# num1 = []
+# for n in num:
+#     num1.append(n**3)
 
-print(num1)
+# print(num1)
+
+# names = ["izhar", "sheryar", "ahmed "]
+# first_letter = []
+# for n in names:
+#     first_letter.append(n[0].title())
+
+# print(first_letter)
+
+num = [n**2 for n in range(1,11)]
+print(num)
+
 
 
